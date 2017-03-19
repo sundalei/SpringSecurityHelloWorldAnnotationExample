db95b0a8f3fd57bd8632e627f834457b904a6700
unknown <>
2017-03-19 21:50:40 +0800

    springsecuritydemo

---
+++ src/main/java/com/websystique/springsecurity/configuration/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+package com.websystique.springsecurity.configuration;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+}
